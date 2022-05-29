617060e6e8e288e16d8685e23cd0101d02f8b047
moldaly <Aida.Moldaly@Administrators-iMac-6.local>
2022-05-29 15:41:08 +0600

    first homeworkAPI2

---
+++ homeworkAPI2/homeworkAPI2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeworkAPI2
+//
+//  Created by Aida Moldaly on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
